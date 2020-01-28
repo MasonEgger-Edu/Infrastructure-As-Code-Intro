00cbbb366a4a6351ca2300156fe2997857f59a51
Mason Egger <mason@masonegger.com>
2020-01-27 19:42:37 -0600

    adding part 4 code

--- part4/main.tf
+++ part4/main.tf
@@ -11,6 +11,10 @@ data "digitalocean_ssh_key" "work" {
     name = "Work Laptop"
 }
 
+data "digitalocean_domain" "web" {
+    name = "egger.codes"
+}
+
 variable "region" {
     type    = string
     default = "nyc3"
@@ -35,13 +39,53 @@ resource "digitalocean_droplet" "web" {
     ssh_keys = [data.digitalocean_ssh_key.home.id, 
         data.digitalocean_ssh_key.work.id]
 
+    user_data = <<EOF
+    #cloud-config
+
+    packages:
+        - nginx
+
+    runcmd:
+        - [ sh, -xc, "echo '<h1>web-${var.region}-${count.index +1}</h1>' >> /var/www/html/index.nginx-debian.html"]
+    EOF
+
     # ensures that we create the new resource before we destroy the old one
     # https://www.terraform.io/docs/configuration/resources.html#lifecycle-lifecycle-customizations
     lifecycle {
         create_before_destroy = true
     }
 }
+resource "digitalocean_loadbalancer" "web" {
+    name        = "web-${var.region}"
+    region      = var.region
+    droplet_ids = digitalocean_droplet.web.*.id
 
-output "server_ip" {
+    forwarding_rule {
+        entry_port = 80
+        entry_protocol = "http"
+
+        target_port = 80
+        target_protocol = "http"
+    }
+
+
+    lifecycle {
+        create_before_destroy = true
+    }
+}
+
+resource "digitalocean_record" "web" {
+    domain = data.digitalocean_domain.web.name
+    type   = "A"
+    name   = var.region
+    value  = digitalocean_loadbalancer.web.ip
+    ttl    = 30
+}
+
+output "servers" {
     value = digitalocean_droplet.web.*.ipv4_address
+}
+
+output "lb" {
+    value = digitalocean_loadbalancer.web.ip
 }
